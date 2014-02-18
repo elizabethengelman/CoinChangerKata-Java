09511a2740386f8b9ed43b88342f365c1d092703
Elizabeth Engelman <emengelman@gmail.com>
2014-02-18 16:38:14 -0600

    first test - 1 cent

--- src/com/coinChanger/CoinChanger.java
+++ src/com/coinChanger/CoinChanger.java
@@ -1,7 +1,14 @@
 package com.coinChanger;
 
+import java.util.ArrayList;
+
 /**
  * Created by elizabethengelman on 2/18/14.
  */
 public class CoinChanger {
+    public ArrayList giveChange(int amount){
+        ArrayList <Integer> change = new ArrayList<Integer>();
+        change.add(1);
+        return change;
+    }
 }
--- tests/CoinChangerTest.java
+++ tests/CoinChangerTest.java
@@ -1,7 +1,28 @@
 package com.coinChanger;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by elizabethengelman on 2/18/14.
  */
 public class CoinChangerTest {
+    private CoinChanger coinChanger;
+    private ArrayList<Integer> expectedChange;
+
+    @Before
+    public void setUp() throws Exception{
+      coinChanger = new CoinChanger();
+    }
+
+    @Test
+    public void givesChangeFor1() throws Exception {
+        ArrayList<Integer> expectedChange = new ArrayList<Integer>();
+        expectedChange.add(1);
+        assertEquals(expectedChange, coinChanger.giveChange(1));
+    }
 }
