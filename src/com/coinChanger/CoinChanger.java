14cd3842930f2dd1e5c27c2e8c08214dc4429e15
Elizabeth Engelman <emengelman@gmail.com>
2014-02-18 16:47:22 -0600

    tested 5 cents

--- src/com/coinChanger/CoinChanger.java
+++ src/com/coinChanger/CoinChanger.java
@@ -8,11 +8,16 @@ import java.util.ArrayList;
 public class CoinChanger {
     public ArrayList giveChange(int amount){
         ArrayList <Integer> change = new ArrayList<Integer>();
+        while (amount >= 5){
+            change.add(5);
+            amount -= 5;
+        }
+
         while (amount > 0){
             change.add(1);
             amount -= 1;
         }
-       
+
         return change;
     }
 }
